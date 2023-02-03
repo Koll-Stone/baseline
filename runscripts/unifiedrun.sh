87cd2c05460f30fe6b7aa05327a01a64f2e0efc9
Ubuntu <ubuntu@ip-172-31-37-242.ap-northeast-2.compute.internal>
2023-02-03 12:16:41 +0000

    update unified running

--- runscripts/unifiedrun.sh
+++ runscripts/unifiedrun.sh
@@ -1,6 +1,18 @@
 #!bin/bash
 echo "unified running starts..."
 
+echo "downloading library..."
+cd /home/ubuntu/xacmlProject
+rm -rf baseline
+git clone https://github.com/Koll-Stone/baseline.git
+
+echo "setting environment..."
+cd /home/ubuntu/xacmlProject/baseline
+mvn install:install-file -Dfile=lib/BFT-SMaRt.jar -DgroupId=org.ulisboa -DartifactId=bftsmart -Dpackaging=jar -Dversion=1.0
+mvn dependency:build-classpath -Dmdep.outputFile=./runscripts/cpcontent
+echo ":$(pwd)/target/classes" >> ./runscripts/cpcontent
+
+echo "running code..."
 myip=$(hostname --ip-address)
 echo "$myip"
 input="/home/ubuntu/xacmlProject/baseline/config/ips"
