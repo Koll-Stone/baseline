67c35cd7582da3889035a3f0008b3e27b822e092
Ubuntu <ubuntu@ip-172-31-37-242.ap-northeast-2.compute.internal>
2023-02-03 12:18:15 +0000

    remove useless files

--- runscripts/paprun.sh
+++
@@ -1,1 +0,0 @@
-gnome-terminal -- /usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/java -Djava.security.properties="./config/java.security" -Dlogback.configurationFile="config/logbackc.xml" -Dfile.encoding=UTF-8 -classpath $(cat "./runscripts/cpcontent") org.example.testclientmanager 1 1 1 20000 &
