139e978976802f931cdf294af9da76d4c714782e
Ubuntu <ubuntu@ip-172-31-37-242.ap-northeast-2.compute.internal>
2023-02-03 13:01:08 +0000

    fix bugs

--- runscripts/setenv.sh
+++ runscripts/setenv.sh
@@ -1,12 +1,15 @@
-echo "start..."
+echo "######start..."
 
+echo "######downloading library..."
+cd /home/ubuntu/xacmlProject
+rm -rf baseline
+git clone https://github.com/Koll-Stone/baseline.git
 
+echo "######setting environment..."
 cd /home/ubuntu/xacmlProject/baseline
-
 mvn install:install-file -Dfile=lib/BFT-SMaRt.jar -DgroupId=org.ulisboa -DartifactId=bftsmart -Dpackaging=jar -Dversion=1.0
-
 mvn dependency:build-classpath -Dmdep.outputFile=./runscripts/cpcontent
-
 echo ":$(pwd)/target/classes" >> ./runscripts/cpcontent
+
 
 echo "finished"
