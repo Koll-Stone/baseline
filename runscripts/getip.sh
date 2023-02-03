6bf95d4047b107cf9e0732efe5839f113c598233
Ubuntu <ubuntu@ip-172-31-35-75.ap-northeast-2.compute.internal>
2023-02-03 08:43:02 +0000

    add unified running script

--- runscripts/getip.sh
+++ runscripts/getip.sh
@@ -1,5 +1,22 @@
+#!bin/bash
+
+
 echo "start"
+echo ""
+
+myip=$(hostname --ip-address)
+echo "$myip"
+
+input="/home/ubuntu/xacmlProject/baseline/config/ips"
+
+me=0
+while read line; do    
+    if [[ $line == $myip ]]
+    then
+        echo "yes me is $me"
+    fi
+    ((me++))
+done < $input
 
 
-
-echo "finished"
+bash runscripts/myrun.sh org.example.baselineServer $me 10000 true
