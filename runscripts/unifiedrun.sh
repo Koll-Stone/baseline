45eba96f095e17d0af178b2915228f7f623944df
Ubuntu <ubuntu@ip-172-31-37-242.ap-northeast-2.compute.internal>
2023-02-16 03:20:55 +0000

    change to 7 replicas

--- runscripts/unifiedrun.sh
+++ runscripts/unifiedrun.sh
@@ -29,7 +29,7 @@ then
     then
         command="runscripts/myrun.sh org.example.baselineServer $me 10000 true true"
         echo $command
-        bash $command
+        # bash $command
     else
         client=$(($me-$servernum))
         start=$(($(($client*1000))+1001))
